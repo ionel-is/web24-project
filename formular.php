41a9a933dbc5c135127f45fa01a14ba8e7bbd139
ionel.s <162845553+ionel-is@users.noreply.github.com>
2024-11-18 01:51:22 +0200

    adaugat pagina de afisare formular

--- formular.php
+++ formular.php
@@ -3,4 +3,12 @@
 $txt = "PHP";
 echo "I love $txt!";
 
+
+<form action="afiasjFormular" method="POST">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+
 ?>
