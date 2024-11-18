9328b172177b6104a5947eb8f9820291f48e9b9f
ionel.s <162845553+ionel-is@users.noreply.github.com>
2024-11-18 02:24:47 +0200

    rectificare link formulare in pagina principala si form show

--- formular.php
+++ formular.php
@@ -4,7 +4,7 @@ $txt = "PHP";
 echo "I love $txt!";
 
 
-<form action="afisajFormular" method="POST">
+<form action="/afisajFormular" method="POST">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
